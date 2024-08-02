6e5f7dd5db78233a61d20065068a553fad8e8127
SomaGarai <soma@pitangent.com>
2024-08-02 18:28:59 +0530

    created Payment table.

---
+++ ChocoLuxAPI/Migrations/20240802113942_addedPaymentTable.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ChocoLuxAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class addedPaymentTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+        name: "TblPayment",
+        columns: table => new
+        {
+            PaymentId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+            OrderId = table.Column<Guid>(type: "uniqueidentifier", nullable: true),
+            UserId = table.Column<string>(type: "nvarchar(450)", nullable: true),
+            PaymentDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+            PaymentType = table.Column<string>(type: "nvarchar(max)", nullable: true),
+            PaymentStatus = table.Column<string>(type: "nvarchar(max)", nullable: true)
+        },
+        constraints: table =>
+        {
+            table.PrimaryKey("PK_TblPayment", x => x.PaymentId);
+            table.ForeignKey(
+                name: "FK_TblPayment_tblOrders_OrderId",
+                column: x => x.OrderId,
+                principalTable: "tblOrders",
+                principalColumn: "OrderId");
+        });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "TblPayment");
+        }
+    }
+}
